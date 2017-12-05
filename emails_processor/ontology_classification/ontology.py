a02029dc5954c27ae6fb654c63e72e1dbbaad687
Kacper Kielak <kielak.kacper@gmail.com>
2017-12-05 17:39:35 +0000

    Add initial keywords to ontology leaves

--- emails_processor/ontology_classification/ontology.py
+++ emails_processor/ontology_classification/ontology.py
@@ -1,28 +1,33 @@
 ontology = {
     'Computer Science': {
         'Software Engineering': {
-
+            'keywords': ['software', 'object-oriented']
         },
 
         'AI': {
-
+            'keywords': ['artificial', 'intelligence', 'machine learning', 'ai', 'robotics', 'vision', 'nlp',
+                         'natural language processing', 'ieee', 'navigation', 'robot', 'humanoid', 'autonomous',
+                         'knowledge', 'language', 'decision',' navigation']
         },
 
         'HCI': {
-
+            'keywords': ['human', 'user', 'friendly', 'interaction', 'graphics', 'fun', 'usable', 'design']
         },
 
         'Parallel Computing': {
-
+            'keywords': ['parallel', 'distributed', 'network', 'synchronization', 'efficient', 'synchronous',
+                         'asynchronous', 'thread', 'multi']
         },
 
         'Cybersecurity': {
-
+            'keywords': ['breach', 'cryptography', 'backdoor', 'encryption', 'decryption', 'hacking']
         },
 
         'Other': {
 
-        }
+        },
+
+        'keywords': ['computer', 'information', 'digital']
     },
 
     'Other': {
