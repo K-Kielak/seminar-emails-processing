10ec00d75584c314abc497e8654b5ac1ff940492
Kacper Kielak <kielak.kacper@gmail.com>
2017-12-05 21:00:59 +0000

    Add wraper class for ontology

--- emails_processor/ontology_classification/ontology.py
+++ emails_processor/ontology_classification/ontology.py
@@ -1,36 +1,103 @@
-ontology = {
-    'Computer Science': {
-        'Software Engineering': {
-            'keywords': ['software', 'object-oriented']
-        },
+import nltk
+from nltk.stem.wordnet import WordNetLemmatizer
+from emails_processor.data_extraction.text_processor import TextProcessor
 
-        'AI': {
-            'keywords': ['artificial', 'intelligence', 'machine learning', 'ai', 'robotics', 'vision', 'nlp',
-                         'natural language processing', 'ieee', 'navigation', 'robot', 'humanoid', 'autonomous',
-                         'knowledge', 'language', 'decision',' navigation']
-        },
+keywords_label = 'keywords'
+seminars_label = 'seminars'
 
-        'HCI': {
-            'keywords': ['human', 'user', 'friendly', 'interaction', 'graphics', 'fun', 'usable', 'design']
-        },
+seminars_ontology = {
+            'Computer Science': {
+                'Software Engineering': {
+                    keywords_label: ['software', 'object-oriented'],
+                    seminars_label: []
+                },
 
-        'Parallel Computing': {
-            'keywords': ['parallel', 'distributed', 'network', 'synchronization', 'efficient', 'synchronous',
-                         'asynchronous', 'thread', 'multi']
-        },
+                'AI': {
+                    keywords_label: ['artificial', 'intelligence', 'machine learning', 'ai', 'robotics', 'vision',
+                                     'nlp', 'natural language processing', 'ieee', 'navigation', 'robot', 'humanoid',
+                                     'autonomous', 'knowledge', 'language', 'decision',' navigation'],
+                    seminars_label: []
+                },
 
-        'Cybersecurity': {
-            'keywords': ['breach', 'cryptography', 'backdoor', 'encryption', 'decryption', 'hacking']
-        },
+                'HCI': {
+                    keywords_label: ['human', 'user', 'friendly', 'interaction', 'graphics', 'fun', 'usable', 'design'],
+                    seminars_label: []
+                },
 
-        'Other': {
+                'Parallel Computing': {
+                    keywords_label: ['parallel', 'distributed', 'network', 'synchronization', 'efficient',
+                                     'synchronous', 'asynchronous', 'thread', 'multi'],
+                    seminars_label: []
+                },
 
-        },
+                'Cyber Security': {
+                    keywords_label: ['breach', 'cryptography', 'backdoor', 'encryption', 'decryption', 'hacking'],
+                    seminars_label: []
+                },
 
-        'keywords': ['computer', 'information', 'digital']
-    },
+                'Other': {
+                    keywords_label: [],
+                    seminars_label: []
+                },
 
-    'Other': {
+                keywords_label: ['computer', 'information', 'digital']
+            },
 
-    }
-}
+            'Other': {
+                keywords_label: []
+            }
+        }
+
+
+class Ontology:
+    def __init__(self, ontology=seminars_ontology):
+        nltk.download('wordnet')
+        self.lemmatizer = WordNetLemmatizer()
+        self.ontology = self._preprocess_ontology(ontology)
+
+    @staticmethod
+    def get_topic_keywords(topic):
+        return topic[keywords_label]
+
+    @staticmethod
+    def get_topic_seminars(topic):
+        return topic[seminars_label]
+
+    def get_ontology(self):
+        return self.ontology
+
+    def get_topic(self, key, ontology=None):
+        if not ontology:
+            ontology = self.ontology
+
+        keys = ontology.keys()
+        if 'keywords' in keys:
+            return None
+
+        if key in keys:
+            return ontology[key]
+
+        for child in keys:
+            topic = self.get_topic(key, ontology[child])
+            if topic:
+                return topic
+
+        return None
+
+    def _preprocess_ontology(self, ontology):
+        for key, obj in ontology.items():
+            if type(obj) is list:
+                continue
+
+            ontology[key] = self._preprocess_ontology(obj)
+            ontology[key][keywords_label] = self._preprocess_keywords(obj[keywords_label])
+
+        return ontology
+
+    def _preprocess_keywords(self, keywords):
+        lemmatized_keywords = [self.lemmatizer.lemmatize(keyword) for keyword in keywords]
+        return [TextProcessor.clean(keyword) for keyword in lemmatized_keywords]
+
+
+
+
